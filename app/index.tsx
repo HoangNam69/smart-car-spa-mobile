84697907852e792ddf2659ce8deb2b057131e595
NAMLEHOANG\lehoa <lehoangnam.31123@gmail.com>
2025-10-28 23:11:39 +0700

    feat(carousel): handle carousel and resolve basic config

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import CarouselCustom from "@/components/carousel/CarouselCustom";
+import { Image, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={["top", "bottom"]}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={require("../assets/images/logo/logo-sider.png")}
+          style={{ width: 200, marginTop: 20 }}
+          resizeMode="contain"
+        />
+        <CarouselCustom />
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ components/carousel/CarouselCustom.tsx
@@ -0,0 +1,48 @@
+import { Dimensions, Image, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+const { width } = Dimensions.get("window");
+const data = [
+  require("../../assets/images/carousel/background01.jpg"),
+  require("../../assets/images/carousel/background02.jpg"),
+  require("../../assets/images/carousel/background03.jpg"),
+  require("../../assets/images/carousel/background04.png"),
+];
+export default function CarouselCustom() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <Carousel
+        width={width - 20}
+        height={200}
+        autoPlay={true}
+        data={data}
+        scrollAnimationDuration={1200}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 10,
+              overflow: "hidden",
+            }}
+          >
+            <Image
+              source={item}
+              style={{
+                width: "100%",
+                height: "100%",
+                resizeMode: "cover",
+              }}
+            />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
